9e310796b3e7bb38348f5813756f315f1f2c67fe
Matt Ghoreishi <sedmgh@gmail.com>
2026-03-11 03:34:01 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
+  title: "Matt Ghoreishi — Mobile & Web Developer",
   author: "Alejandro Múnez Cuntez",
   description:
     "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
@@ -25,7 +25,7 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
+    name: "Matt Ghoreishi",
     specialty: "Mobile & Web Developer",
     summary:
       "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
